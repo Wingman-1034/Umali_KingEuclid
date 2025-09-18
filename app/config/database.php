08c4df076ae0bedcceb7b7844ab48a42c0d0e71f
Wingman_1034 <kingumali63@gmail.com>
2025-09-18 16:25:04 +0800

    updated

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'shoeniverse_lava',
+    'username'	=> 'sql12799002',
+    'password'	=> 'ihyDCZJFDm',
+    'database'	=> 'sql12799002',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
